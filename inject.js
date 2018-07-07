6ab40b1c2ce21d6e74c70a1bceb79f6777a8718b
Andrew VanNess <avanness@student.bridgew.edu>
2018-07-06 23:14:11 -0400

    add colorblind function

--- inject.js
+++ inject.js
@@ -2,10 +2,20 @@
 
 (function() {
 
+// colorblind mode
+	(function () {
+		var body = document.body;
+		body.style['filter'] = 'progid:DXImageTransform.Microsoft.BasicImage(grayscale=1)';
+		if (!body.style['filter']) {
+		  body.style['-webkit-filter'] = 'grayscale(1)';
+		  body.style['filter'] = 'grayscale(1)';
+		}
+	  }());
+
 
 	document.querySelectorAll('*').forEach(function(node) {
 		console.log(node)
-		
+
 	});
 
 
