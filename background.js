29a8a0663f2a3b4e121260080c072ba823fc2434
Andrew VanNess <Andrew.VanNess@erg.com>
2018-07-11 12:41:15 -0400

    add icons, tabs, color scheme, jquery, UI skeleton

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 // this is the background code...
 
 // listen for our browerAction to be clicked
-chrome.browserAction.onClicked.addListener(function (tab) {
-	// for the current tab, inject the "inject.js" file & execute it
-	chrome.tabs.executeScript(tab.ib, {
-		file: 'inject.js'
-	});
-});
+// chrome.browserAction.onClicked.addListener(function (tab) {
+// 	// for the current tab, inject the "inject.js" file & execute it
+// 	chrome.tabs.executeScript(tab.ib, {
+// 		file: 'inject.js'
+// 	});
+// });
+
