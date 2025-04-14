5d78d70f84fb2053c242df3f4808970e8ac75d51
Andrew VanNess <andrewvanness221@gmail.com>
2025-04-14 00:31:07 -0400

    fix permissions issues

--- script/colorblindListener.js
+++ script/colorblindListener.js
@@ -5,60 +5,19 @@
 // over options, the currently selected filter will still be applied to the popup
 window.selectedFilter = null;
 
-/**
- * get the selected filter on popup open
- */
-window.onload = function() {
-  if (!chrome || !chrome.storage || !chrome.storage.local) return;
-  chrome.storage.local.get(["key"], function(result) {
-    try {
-      document.getElementById(result.key).click();
-    } catch (e) {
-      console.log(e);
-    }
-  });
-};
-
-/**
- * Sets the selected filter in storage
- * @param {String} value the selected input
- */
-function setSelected(value) {
-  try {
-    chrome.storage.local.set({ key: value }, function() {
-      document.getElementById(value).checked = true;
-    });
-  } catch {}
-}
-
 async function injectFilter(fileName) {
   const tab = await chrome.tabs.query({ active: true });
   if (tab.length === 0) return;
   chrome.scripting.executeScript({
-    target: {tabId: tab[0].id},
-    files: [fileName]
+    target: { tabId: tab[0].id },
+    files: [fileName],
   });
-
-  /* All tabs at once method. Requires permission to every domain. 
-  Better to restrict to current tab. */ 
-
-  // const tabs = await chrome.tabs.query({});
-  // console.log('all tabs:',tabs)
-  // tabs.forEach(tab => {
-  //   console.log('tab...', tab)
-  //   chrome.scripting.executeScript({
-  //     target: {tabId: tab.id},
-  //     files: [fileName]
-  //   });
-  // });
-
 }
 
-document.querySelectorAll(['[id^="radio"]']).forEach(radioButton => {
+document.querySelectorAll(['[id^="radio"]']).forEach((radioButton) => {
   const filter = radioButton.parentElement.id.replace("option-", "");
-  radioButton.addEventListener("click", function() {
-    // page-specific filters
-    setSelected(radioButton.id);
+  radioButton.addEventListener("click", function () {
+    // inject the filter script
     injectFilter(`filters/${filter}.js`);
     // popup-specific filters
     applyFilter((window.selectedFilter = filter));
